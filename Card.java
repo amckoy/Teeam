dc5fc3b5c211e781de067ca8697c46186fe243fc
dlin8 <dlin8@stuy.edu>
2016-06-02 11:34:23 -0400

    finished card.java

--- Card.java
+++ Card.java
@@ -1,10 +1,46 @@
 /* The basic card class */
 
-public abstract class Card {
+public class Card {
     
-    int atk;
-    int hp;
+    int attack;
+    int health;
     int cost;
     String name;
+    
+    Card(){
+	attack = 0;
+	health = 1;
+	cost = 0;
+	name = "";
+    }
+    
+    // Mutators + Accessors
+    protected int getAttack(){
+	return attack;
+    }
+
+    protected int getHealth(){
+	return health;
+    }
+
+    protected int getCost(){
+	return cost;
+    }
+
+    protected String getName(){
+	return name;
+    }
+
+    protected void setAttack( int newAttack ){
+	attack = newAttack;
+    }
+
+    protected void setHealth( int newHealth ){
+	health = newHealth;
+    }
+
+    protected void setCost( int newCode ){
+	code = newCode;
+    }
 
 }
