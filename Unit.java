1e7bec2134a1650b8d3685abad00bc5171595b2a
dlin8 <dlin8@stuy.edu>
2016-06-02 11:55:02 -0400

    updated Unit.java

--- Unit.java
+++ Unit.java
@@ -1,4 +1,18 @@
 public Unit class extends Card{
     
-    
+    protected int attackUnit( Unit enemy ){
+	enemy.setHealth( enemy.getHealth() -= this.attack() );
+	this.setHealth( this.Health -= enemy.getAttack() );
+	return this.attack();
+    }
+
+    protected int attackPlayer(){
+	/* */;
+    }
+
+    protected void checkHealth(){
+	if( health <= 0 ){
+	    /* */;
+	}
+    }
 }
